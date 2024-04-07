9e2b3aadaa4058267e18ab71f0e0d17117d94ffc
Aaron Mayhua <113055771+AaronMayhua@users.noreply.github.com>
2024-04-06 22:37:48 -0500

    Mi Primer Commit

---
+++ src/main/java/com/aaron/SpringGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.aaron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitHubApplication.class, args);
+	}
+
+}
